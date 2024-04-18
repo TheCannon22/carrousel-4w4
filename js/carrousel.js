6f7bc284942ca76374e179539089898d9548e1a4
Alexander Rankov <e2259198@cmaisonneuve.qc.ca>
2024-04-18 16:06:49 -0400

    S10C2: On a fait en sorte qu'on peut faire apparaître la page de la carrousel en cliquant sur le bouton et on peut la fermer aussi

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,32 @@
-(function(){
+(function () {
     console.log("Début du carrousel");
+    let carrousel = document.querySelector('.carrousel');
+    console.log(" carrousel : ", carrousel.tagName);
+    let bouton = document.querySelector('.bouton__ouvrir');
+    console.log(" bouton : ", bouton.tagName);
+    let carrousel__x = document.querySelector('.carrousel__x');
+
+    let galerie = document.querySelector('.galerie');
+    // let galerie__image = galerie.querySelector('img'); // Récupère la première image de la galerie
+
+
+    let carrousel__figure = document.querySelector('.carrousel__figure');
+    let galerie__image = galerie.querySelectorAll('img'); // Récupère toutes les images de la galerie
+
+    for (const elm of galerie__image) {
+        let carrousel__image = document.createElement('img');
+        carrousel__image.classList.add('carrousel__image');
+        console.log(elm.src);
+        carrousel__image.src = elm.src;
+        console.log(carrousel__image.src);
+        carrousel__figure.appendChild(carrousel__image);
+    }
+
+    bouton.addEventListener('mousedown', function () {
+        carrousel.classList.add('carrousel--ouvrir');
+    })
+
+    carrousel__x.addEventListener('mousedown', function () {
+        carrousel.classList.remove('carrousel--ouvrir');
+    })
 })()
