219a69e541ebb367e8c81a9a7a64d70b8e5a6ff1
Alexander Rankov <e2259198@cmaisonneuve.qc.ca>
2024-04-18 16:38:35 -0400

    S10C2: J'ai ajouté le code nécessaire pour que les boutons radios s'affichent et pour que quand un bouton s'affiche, que la prochaine image s'affiche

--- js/carrousel.js
+++ js/carrousel.js
@@ -7,20 +7,41 @@
     let carrousel__x = document.querySelector('.carrousel__x');
 
     let galerie = document.querySelector('.galerie');
-    // let galerie__image = galerie.querySelector('img'); // Récupère la première image de la galerie
-
-
     let carrousel__figure = document.querySelector('.carrousel__figure');
     let galerie__image = galerie.querySelectorAll('img'); // Récupère toutes les images de la galerie
 
-    for (const elm of galerie__image) {
+    let radioContainer = document.createElement('div');
+    radioContainer.classList.add('radio-container');
+
+    for (let i = 0; i < galerie__image.length; i++) {
         let carrousel__image = document.createElement('img');
         carrousel__image.classList.add('carrousel__image');
-        console.log(elm.src);
-        carrousel__image.src = elm.src;
-        console.log(carrousel__image.src);
+        carrousel__image.src = galerie__image[i].src;
         carrousel__figure.appendChild(carrousel__image);
+    
+        // Create radio button for each image
+        let radioButton = document.createElement('input');
+        radioButton.type = 'radio';
+        radioButton.name = 'carousel-radio';
+        radioButton.id = 'radio' + i;
+        radioButton.classList.add('carousel-radio');
+        radioButton.value = i;
+        radioContainer.appendChild(radioButton);
+    
+        // Add click event listener to radio button
+        radioButton.addEventListener('click', function() {
+            // Hide all images
+            let allImages = carrousel__figure.querySelectorAll('.carrousel__image');
+            allImages.forEach(img => img.style.display = 'none');
+    
+            // Show clicked image
+            carrousel__image.style.display = 'block';
+        });
     }
+    
+    carrousel.appendChild(radioContainer);
+
+    carrousel.appendChild(radioContainer);
 
     bouton.addEventListener('mousedown', function () {
         carrousel.classList.add('carrousel--ouvrir');
